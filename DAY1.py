c3e14d6299246ffe993f017dccf91c4e466eafbd
TarushChintala <101618684+TarushChintala@users.noreply.github.com>
2024-09-10 15:08:52 +0530

    Upload of day1

---
+++ DAY1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("What city did you grow up in?\n")
+animal = input("What is your favourite animal?\n")
+print("Your band name is " + city + " " + animal)
